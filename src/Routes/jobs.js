f7663c916a061a099d9d0b345a1a96a19ae9717a
Srujith Madadi <srujith.madadi@walmart.com>
2021-02-28 02:02:24 -0600

    adding a dummy route

--- src/Routes/jobs.js
+++ src/Routes/jobs.js
@@ -2,6 +2,9 @@ const express=require("express");
 const app=express();
 const Jobs=require("../controllers/jobs")
 
+app.get("/",(req,res)=>{
+    res.status(200).send({status:"Egen-jobportal-node-server is running on port "+process.env.PORT})
+})
 app.get("/getJobs",Jobs.getJobs);
 app.get("/getJobDetails",Jobs.getJobDetails)
 
